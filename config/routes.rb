2c8717276b6c69897e0bcd5b2876332b5430a022
TakutoMuranaga <takutomura2019@gmail.com>
2022-09-19 07:56:45 +0000

    logsignin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  get 'homes/top'
   devise_for :admins
   devise_for :customers
+
+  namespace :admin do
+    resources :items
+
+  end
+
+  scope module: :public do
+    root to: "homes#top"
+    resources :items
+    resources :orders, only: [:new, :confirm, :thanks, :create, :index, :show ]
+  end
+  #あとで消す
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
