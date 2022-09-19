24e6f6a1b95cc4433d35e88670c80f90ab64b3fc
EC2 Default User <ec2-user@ip-172-31-47-99.ap-northeast-1.compute.internal>
2022-09-19 06:53:45 +0000

    top&about1

---
+++ app/controllers/public/homes_controller.rb
@@ -0,0 +1,10 @@
+class Public::HomesController < ApplicationController
+    
+    def top
+    end
+    
+    # def index
+    #     @items = items.all.order(created_at: :desc)
+    # end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  
   devise_for :admins
   devise_for :customers
+  
+  namespace :admin do
+  resources :items
+  
+  end
 
+  scope module: :public do
+    
+  get 'homes/top'
+  get 'homes/about'
+  
+  resources :items
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
