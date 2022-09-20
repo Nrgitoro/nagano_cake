2c9bc33f8af4aeab7ec7ab2655e70cfbbd54616f
EC2 Default User <ec2-user@ip-172-31-32-14.ap-northeast-1.compute.internal>
2022-09-20 08:35:24 +0000

    genre

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  
   devise_for :admins
   devise_for :customers
+  
+ 
+  
+  namespace :admin do
+    resources :items
+    resources :genres, only: [:index,:edit,:create,:update]
+
+  end
+
+  scope module: :public do
+    resources :items
+
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  end
