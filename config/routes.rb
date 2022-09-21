db2a25d488d7f6685cd2f86ee67291eb3002944a
EC2 Default User <ec2-user@ip-172-31-19-92.ap-northeast-1.compute.internal>
2022-09-21 08:57:27 +0000

    device

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
-  devise_for :customers
+  devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
+devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
   
   namespace :admin do
     resources :items, only: [:edit, :index, :new, :show, :create, :update]
