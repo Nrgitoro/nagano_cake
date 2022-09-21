6c999f84f77ffd682879975be83326a24df39b30
TakutoMuranaga <takutomura2019@gmail.com>
2022-09-21 09:13:34 +0000

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ devise_for :customers, controllers: {
   scope module: :public do
 
   resources :deliveries, only: [:create, :index, :show, :edit, :update, :destroy]
-  end
+  
 
     root to: "homes#top"
     get 'homes/about'
@@ -30,19 +30,6 @@ devise_for :customers, controllers: {
     get 'orders/thanks' => "orders#thanks"
   end
   #あとで消す
-
-  namespace :admin do
-  resources :items
-
-  end
-
-  scope module: :public do
-    
-  get 'items/show'
-  get 'items/index'
-    
-  resources :items
-  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
