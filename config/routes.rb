af4892e8625b0f9cb5af205bb715f2eb1661f107
EC2 Default User <ec2-user@ip-172-31-19-92.ap-northeast-1.compute.internal>
2022-09-21 05:21:13 +0000

    item-first

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+
   devise_for :admins
   devise_for :customers
 
+  namespace :admin do
+
+  resources :items, only: [:edit, :index, :new, :show, :create, :update]
+
+  end
+
+  scope module: :public do
+
+  resources :items, only: [:index, :show]
+
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
