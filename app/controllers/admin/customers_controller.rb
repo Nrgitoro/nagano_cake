af73456e6c11988b5b393e22719812813fd14f64
EC2 Default User <ec2-user@ip-172-31-32-14.ap-northeast-1.compute.internal>
2022-09-23 07:56:29 +0000

    3回目

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,12 +4,23 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
-    @customers = Customer.all
     @customer =Customer.find(params[:id])
   end
 
   def edit
-    @customers = Customer.all
     @customer =Customer.find(params[:id])
   end
+
+   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path
+   end
+   
+    private
+
+  def customer_params
+    params.require(:customer).permit(:name_first, :name_second, :name_firdt_rubi, :name_second_rubi, :address, :tel, :post_code, :email)
+  end
+
 end
