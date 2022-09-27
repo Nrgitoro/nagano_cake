6fd9141819097daba5403a3d08a3c4bafd700ca8
EC2 Default User <ec2-user@ip-172-31-19-92.ap-northeast-1.compute.internal>
2022-09-27 07:46:35 +0000

    select

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,9 @@
 class Admin::OrdersController < ApplicationController
   def show
-    @order = Order.find(params[:id])
+    @order = Order.find_by(id: params[:id])
     @order_items = @order.order_details
-   
-    
-  
+
+
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2022_09_19_121532) do
   end
 
   create_table "cart_items", force: :cascade do |t|
+    t.integer "amount", null: false
     t.integer "item_id", null: false
     t.integer "customer_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -109,7 +110,6 @@ ActiveRecord::Schema.define(version: 2022_09_19_121532) do
     t.integer "order_id", null: false
     t.integer "price", null: false
     t.integer "amount", null: false
-    t.integer "making_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -122,6 +122,7 @@ ActiveRecord::Schema.define(version: 2022_09_19_121532) do
     t.integer "shopping_cost", default: 800, null: false
     t.integer "billing", null: false
     t.integer "payment_method", null: false
+    t.boolean "order_status", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
